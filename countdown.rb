454ddc08cea1e763ad0296052dc321ebb3f5afd3
Kimberly <kmglock@mail.lipscomb.edu>
2020-12-13 19:27:01 -0600

    commit tree 9f4dd0ae0769f5f501139f677f57e1d383d43ff7

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
-#write your code here
 
-def countdown
+
+def countdown(x) # takes an integer argument
+    while x > 0
+        puts "#{x} SECOND(S)!" # prints x each run of loop
+        x -= 1 # takes 1 away from variable x each run of the loop
+    end # loops ends when x is 0
+    return "HAPPY NEW YEAR!" 
+    #puts "HAPPY NEW YEAR!" # ruby prints last thing in method and want to print this after the x countdown above is finished so put after loop ends
+end # ran in irb and is good!
+
+
+def countdown_with_sleep(x)
+    while x > 0
+        puts "#{x} SECOND(S)!"
+        sleep 1 # makes code wait for one second until it runs the next time so countdown has 1 second between each second:)
+        x -= 1
+    end
+    return "HAPPY NEW YEAR!"
+    #puts "HAPPY NEW YEAR!"
 end
